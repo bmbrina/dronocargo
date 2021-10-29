edfc88599044b1b84e1b701a514734e36161cdc0
Barbara Brina <barbara.brina@armory.io>
2021-10-29 10:27:53 -0500

    Deletes unused file

--- src/utils/asyncLocalStorage.ts
+++
@@ -1,8 +0,0 @@
-export const asyncLocalStorage = {
-  async setItem (key: any, value: any) {
-      return localStorage.setItem(key, value);
-  },
-  async getItem (key: any) {
-      return localStorage.getItem(key);
-  }
-}
