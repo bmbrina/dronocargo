2bb557ae8e7998ff0b9de2ac72c54b1a6538fd78
Barbara Brina <barbara.brina@armory.io>
2021-10-29 10:31:56 -0500

    Adds header and footer skeleton components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Footer from './components/Footer'
+import Header from './components/Header'
 import * as Routes from './routes'
 
 const App = () => (
     <div className="App">
       <BrowserRouter>
-        {/* <Header /> */}
+        <Header />
         <Switch>
           <Route exact path="/shipments" component={Routes.Shipments} />
         </Switch>
-        {/* <Footer /> */}
+        <Footer />
       </BrowserRouter>
     </div>
   )
